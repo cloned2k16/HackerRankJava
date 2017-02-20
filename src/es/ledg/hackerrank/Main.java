a3830f7deeca4e30c20b726711e836333fb32ca3
i5 <know@ledg.es>
2017-02-20 18:15:42 +0100

    initial commit ..

---
+++ src/es/ledg/hackerrank/Main.java
@@ -0,0 +1,89 @@
+package es.ledg.hackerrank;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+
+//------------------------------------- --------------------------- --------------------------------
+	public class 						Main 
+	extends								BaseObject					{
+		
+
+		static 
+		public	void					main						( String args[] )				{
+			Main me = new Main();
+			me._main(args);
+		}
+		
+				
+		public void						_main						(String args[]){
+			
+			try{
+				doTest ("SherlockPairs", new int [] { 0 , 3} );
+			}
+			catch (Exception e){
+				_log("Exception %s",e.getMessage());
+			}
+		}
+		
+		void							doTest						(String className, int [] tests) 
+				throws 					Exception													{
+			
+			if (tests==null) throw new Exception ("Invalid argument! tests can't be null!");
+
+			String packageName		= "es.ledg.hackerrank";
+			String fullPackageName	= packageName+"."+className;
+			String fullClassName	= fullPackageName+".Solution";
+			
+			Class<?> []	paramsType		=  { String[].class };
+			try {
+				
+								classe 	 = Class.forName(fullClassName);
+				Method 			testMthd = classe.getMethod("main", paramsType);
+				int nt=tests.length;
+				for (int tn=0; tn<nt; tn++){
+					String tN=String.format("%02d", tests[tn]);
+					String 			basePath=  "bin/"+fullPackageName.replace('.', '/')+"/";
+					String          fileOut =	String.format(basePath+"output%s.txt"	, tN);
+					String          fileRes =	String.format(basePath+"res%s.txt"		, tN);
+					String          fileIn	= 	String.format(basePath+"input%s.txt"	, tN);
+					System.setOut	(new PrintStream(new FileOutputStream(new File(fileRes))));
+					System.setIn	(new FileInputStream(new File(fileIn)));
+					testMthd.invoke(null,new Object [] { new String[] {}});
+					System.setOut	(new PrintStream(new FileOutputStream(FileDescriptor.out)));		
+					System.setIn	(new BufferedInputStream(new FileInputStream(FileDescriptor.in)));
+					
+					BufferedReader fOut=new BufferedReader(new FileReader(fileOut));
+					BufferedReader fRes=new BufferedReader(new FileReader(fileRes));
+					String line;
+					int ln=0;
+					boolean errorAny=false;
+					while (null!= (line = fOut.readLine())){
+				      String rLine=fRes.readLine();
+				      if (!line.equals(rLine)){
+				    	  _log("Error at line: %d res: '%s' != out: '%s'",ln,rLine,line);
+				    	  errorAny=true;
+				      }
+				      ln++;
+					}
+					fOut.close();
+					fRes.close();
+					if (errorAny)	_log("[%s] Error!"	,tN);
+					else			_log("[%s] Done."	,tN);
+				}
+			}
+			catch 		( ClassNotFoundException 	e) 		{ _log(e); }
+			catch 		( NoSuchMethodException 	e) 		{ _log(e); } 
+			catch 		( SecurityException 		e) 		{ _log(e); } 
+		    catch 		(Throwable t) 						{ _log(t); }
+			
+			
+		}
+	}
+//------------------------------------- --------------------------- --------------------------------
---
+++ src/es/ledg/hackerrank/SherlockPairs/Solution.java
@@ -0,0 +1,59 @@
+package es.ledg.hackerrank.SherlockPairs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+import es.ledg.hackerrank.SolutionBase;
+
+//------------------------------------- --------------------------- --------------------------------
+	public class 						Solution 					
+	extends								SolutionBase												{
+
+
+		private static long 			getPairs					(int[] a) 						{
+			long sum = 0;
+            int  len = a.length;
+            for ( int i = 1 ; i < len  ; i++ ) {
+                long c = 1;
+                int p=a[i-1],p1;
+                while ( i < len && (p1=a [ i ]) == p ) {
+                    c++;
+                    p=p1;
+                    i++;
+                }
+                sum +=  (c * ( c-1 )); 
+            }
+            return sum;			
+		}
+
+		public 
+		static 
+		void 							main						(String[] args) 				{
+			try {
+				BufferedReader inB = new BufferedReader(new InputStreamReader(System.in));
+				int T = Integer.parseInt(inB.readLine());
+				
+				for (int tn=0; tn<T; tn++){
+					 int N = Integer.parseInt(inB.readLine());
+			            int[] A = new int[N];
+			            int n = 0;
+			            for (String as: inB.readLine().split(" ")){
+			                A[n++] = Integer.parseInt(as);
+			            }
+			            if (n!=N) throw new Exception ("Invalid input!");
+			            Arrays.sort(A);
+			            
+			            long res=getPairs(A);
+			            System.out.println(res);
+				}
+			}
+			catch(Exception e){
+				_out("Exception: %s",e.getMessage());
+			}
+		}
+
+		
+
+	}
+//------------------------------------- --------------------------- --------------------------------
